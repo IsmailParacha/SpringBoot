75bfc81f428c0abced428a6a62e8492ec7645339
IsmailParacha <ismailjanparacha@gmail.com>
2022-09-23 17:19:58 +0500

    Table created

---
+++ src/main/java/com/blog/entities/User.java
@@ -0,0 +1,20 @@
+package com.blog.entities;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String about;
+
+}

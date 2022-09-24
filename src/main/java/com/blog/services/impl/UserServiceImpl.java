a6ef3b643608770915e28fd919ad6c8700368cd8
IsmailParacha <ismailjanparacha@gmail.com>
2022-09-25 00:10:19 +0500

    Creating DTO to User method

--- src/main/java/com/blog/services/impl/UserServiceImpl.java
+++ src/main/java/com/blog/services/impl/UserServiceImpl.java
@@ -2,15 +2,22 @@ package com.blog.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.blog.entities.User;
 import com.blog.payload.UserDto;
+import com.blog.repository.userRepo;
 import com.blog.services.UserService;
 
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private userRepo userrepo;
 
     @Override
-    public UserDto createUser(UserDto user) {
-        // TODO Auto-generated method stub
-        return null;
+    public UserDto createUser(UserDto userDto) {
+        User user = this.dtoToUser(userDto);
+        User savedUser = this.userrepo.save(user);
+        return this.userToDto(savedUser);
     }
 
     @Override
@@ -35,6 +42,24 @@ public class UserServiceImpl implements UserService {
     public void deleteUser(Integer id) {
         // TODO Auto-generated method stub
 
+    }
+
+    private User dtoToUser(UserDto dtoObj) {
+        User user = new User();
+        user.setId(dtoObj.getId());
+        user.setName(dtoObj.getName());
+        user.setEmail(dtoObj.getEmail());
+        user.setAbout(dtoObj.getAbout());
+        return user;
+    }
+
+    private UserDto userToDto(User userObj) {
+        UserDto user = new UserDto();
+        user.setId(userObj.getId());
+        user.setName(userObj.getName());
+        user.setEmail(userObj.getEmail());
+        user.setAbout(userObj.getAbout());
+        return user;
     }
 
 }

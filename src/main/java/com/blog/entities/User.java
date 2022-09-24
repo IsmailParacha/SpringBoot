a1854948f62dd5eab27ba1b7d2e069d7f189289c
IsmailParacha <ismailjanparacha@gmail.com>
2022-09-24 23:11:34 +0500

    Spring

--- src/main/java/com/blog/entities/User.java
+++ src/main/java/com/blog/entities/User.java
@@ -4,14 +4,15 @@ import javax.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	@Column(nullable = false)
 	private String name;
 	private String email;
 	private String password;
--- src/main/java/com/blog/services/UserService.java
+++ src/main/java/com/blog/services/UserService.java
@@ -1,5 +1,8 @@
 package com.blog.services;
 
+import com.blog.entities.User;
+
 public interface UserService {
+    // User createUser(User user);
 
 }

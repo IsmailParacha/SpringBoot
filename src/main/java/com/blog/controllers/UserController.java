04be571fef0558b6332dba36b7c2cf96d6f24bce
IsmailParacha <ismailjanparacha@gmail.com>
2022-09-26 21:54:21 +0500

    Update user controller

--- src/main/java/com/blog/controllers/UserController.java
+++ src/main/java/com/blog/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.blog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,14 @@ public class UserController {
         UserDto createUserDto = this.userServices.createUser(userdto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
+
     // PUT-update user
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userdto, @PathVariable Integer id) {
+        UserDto updateUser = this.userServices.updateUser(userdto, id);
+        return ResponseEntity.ok(updateUser);
+
+    }
     // DELETE-delete user
     // GET-user get
 

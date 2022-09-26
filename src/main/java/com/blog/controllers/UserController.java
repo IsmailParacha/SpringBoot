87db5919a704dcaf32597e99c379ad7e88aa7137
IsmailParacha <ismailjanparacha@gmail.com>
2022-09-26 22:02:09 +0500

    User APi Successful Created

--- src/main/java/com/blog/controllers/UserController.java
+++ src/main/java/com/blog/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blog.payload.ApiResponse;
 import com.blog.payload.UserDto;
 import com.blog.services.UserService;
 
@@ -33,7 +38,24 @@ public class UserController {
         return ResponseEntity.ok(updateUser);
 
     }
+
     // DELETE-delete user
-    // GET-user get
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer id) {
+        this.userServices.deleteUser(id);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted successfully", true), HttpStatus.OK);
+    }
+
+    // GET- multiple users get
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(this.userServices.getAllUsers());
+    }
+
+    // GET- single user get
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getSingleUsers(@PathVariable Integer id) {
+        return ResponseEntity.ok(this.userServices.getUserById(id));
+    }
 
 }

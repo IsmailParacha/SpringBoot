238995611c415ee48b4692294f310fa7247e17e2
Ismail Jan Paracha <ismailjanparacha@gmail.com>
2022-10-04 12:42:09 -0700

    CategoryDTO valiadtion

--- src/main/java/com/blog/payload/CategoryDto.java
+++ src/main/java/com/blog/payload/CategoryDto.java
@@ -13,8 +13,9 @@ import lombok.Setter;
 public class CategoryDto {
     private int catid;
     @NotEmpty
-    @Size(min = 4, max = 100, message = "Title must  be min 4 Chars")
+    @Size(min = 4, max = 100, message = "Title must  be min 4 Chars!")
     private String title;
     @NotEmpty
+    @Size(min = 6, max = 100, message = "Description must  be min 6 Chars!")
     private String description;
 }

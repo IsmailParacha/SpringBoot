cad167267f1da85b954c222cb2b12313c8087ec7
Ismail Jan Paracha <ismailjanparacha@gmail.com>
2022-10-04 12:40:59 -0700

    post validation

--- src/main/java/com/blog/payload/PostDto.java
+++ src/main/java/com/blog/payload/PostDto.java
@@ -2,6 +2,7 @@ package com.blog.payload;
 
 import java.util.Date;
 
+import javax.validation.constraints.*;
 
 import lombok.*;
 
@@ -10,7 +11,10 @@ import lombok.*;
 @NoArgsConstructor
 
 public class PostDto {
+    @NotEmpty
+    @Size(min = 5, message = "Title must be minimum 5 chars!")
     private String title;
+    @Size(min = 5, message = "Content must be minimum 5 chars!")
     private String content;
     private String imageName;
     private Date addedDate;

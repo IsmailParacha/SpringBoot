7b696c78e874ff119f3dbfc371abe670f3a52fc2
Ismail Jan Paracha <ismailjanparacha@gmail.com>
2022-10-06 15:37:02 -0700

    Post get by USerID & CategoryID controller

--- src/main/java/com/blog/controllers/postController.java
+++ src/main/java/com/blog/controllers/postController.java
@@ -1,11 +1,13 @@
 package com.blog.controllers;
 
 import javax.validation.Valid;
-
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
 import com.blog.payload.PostDto;
 import com.blog.services.PostServices;
 
+
 @RestController
 @RequestMapping("/api/")
 public class postController {
     @Autowired
     private PostServices postservice;
 
-    @RequestMapping("/user:{userId}/category:{categoryId}")
+    @PostMapping("user:{userId}/category:{categoryId}")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postdto, @PathVariable Integer userId,@PathVariable Integer categoryId)
     {
        PostDto createPost= this.postservice.createPost(postdto, userId, categoryId);
        return new ResponseEntity<>(createPost,HttpStatus.CREATED);
     }
-    
+
+    @GetMapping("/user:{userId}/posts")
+    public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable Integer userId)
+    {
+        List<PostDto> posts=this.postservice.getPostByUser(userId);
+        return new ResponseEntity<List<PostDto>>(posts,HttpStatus.OK);
+    }
+    @GetMapping("/category:{catId}/posts")
+    public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable Integer catId)
+    {
+        List<PostDto> posts= this.postservice.getPostByCategory(catId);
+        return new ResponseEntity<List<PostDto>>(posts,HttpStatus.OK);
+    }
 }

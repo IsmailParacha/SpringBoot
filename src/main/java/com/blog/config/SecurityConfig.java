c524a4903ed18bff63ea3b258b24258320c9ad36
Ismail Jan Paracha <ismailjanparacha@gmail.com>
2022-10-17 11:09:10 -0700

    API documentation through swagger

--- src/main/java/com/blog/config/SecurityConfig.java
+++ src/main/java/com/blog/config/SecurityConfig.java
@@ -16,15 +16,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.blog.Security.CustomUserDetailService;
 import com.blog.Security.JwtAuthenticationEntryPoint;
 import com.blog.Security.JwtAuthenticationFilter;
 
 @Configuration
+@EnableWebMvc
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    public static final String[] PUBLIC_URLS={
+        "/api/v1/auth/**",
+        "/v3/api-docs",
+        "/v2/api-docs",
+        "/swagger-resources/**",
+        "/swagger-ui/**",
+        "/webjars/**"
+
+    };
     @Autowired
     private CustomUserDetailService customUserDetailService;
     @Autowired
@@ -38,8 +49,9 @@ public class SecurityConfig {
         http.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers(PUBLIC_URLS).permitAll()
                 .antMatchers(HttpMethod.GET).permitAll()// remove security from all GET APi
+               
                 .anyRequest()
                 .authenticated()
                 .and()
